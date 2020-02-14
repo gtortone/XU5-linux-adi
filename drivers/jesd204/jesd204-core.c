analogdevicesinc/linux

jesd204: add jesd204_{to,from}_device() accessors
These will be typically used by users of the framework to convert a
`jesd204_dev` struct to `device` and back.
Useful, when trying to dig-out private data.

Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

drivers/jesd204/jesd204-core.c
@@ -28,6 +28,28 @@ static inline bool dev_is_jesd204_dev(struct device *dev)
 	return device_property_read_bool(dev, "jesd204-device");
 }
 
+struct jesd204_dev *jesd204_dev_from_device(struct device *dev)
+{
+	struct jesd204_dev *jdev;
+
+	if (!dev)
+		return NULL;
+
+	list_for_each_entry(jdev, &jesd204_device_list, list) {
+		if (jdev->dev && jdev->dev == dev)
+			return jdev;
+	}
+
+	return NULL;
+}
+EXPORT_SYMBOL(jesd204_dev_from_device);
+
+struct device *jesd204_dev_to_device(struct jesd204_dev *jdev)
+{
+	return jdev ? jdev->dev : NULL;
+}
+EXPORT_SYMBOL(jesd204_dev_to_device);
+
 static struct jesd204_dev *jesd204_dev_alloc(struct device_node *np)
 {
 	struct jesd204_dev_top *jdev_top;

gtortone/XU5-linux-adi

(cherry picked from commit 1b6a3047db8bbc00301e904397a228447c25c172)

drivers/jesd204/jesd204-core.c
@@ -37,6 +37,28 @@ static inline bool dev_is_jesd204_dev(struct device *dev)
 	return device_property_read_bool(dev, "jesd204-device");
 }
 
+struct jesd204_dev *jesd204_dev_from_device(struct device *dev)
+{
+	struct jesd204_dev *jdev;
+
+	if (!dev)
+		return NULL;
+
+	list_for_each_entry(jdev, &jesd204_device_list, entry) {
+		if (jdev->dev && jdev->dev == dev)
+			return jdev;
+	}
+
+	return NULL;
+}
+EXPORT_SYMBOL(jesd204_dev_from_device);
+
+struct device *jesd204_dev_to_device(struct jesd204_dev *jdev)
+{
+	return jdev ? jdev->dev : NULL;
+}
+EXPORT_SYMBOL(jesd204_dev_to_device);
+
 static struct jesd204_dev *jesd204_dev_alloc(struct device_node *np)
 {
 	struct jesd204_dev_top *jdev_top;
analogdevicesinc/linux

jesd204: add state transitions needed to start JESD204 links
All state (after the PROBED state) are mostly:
1. "propagate this CB to all devices"
    - if any CB gives an error -> stop ; else -> continue
    - if a CB is defined (for a device) and returns
      JESD204_STATE_CHANGE_DONE, then all is fine (from this device's point
      of view to transition)
    - if a CB returns JESD204_STATE_CHANGE_DEFER, it means that the device
      is not ready to transition, and should come back when it is ready;
      this come-back is not yet implemented, which means that the whole of
      the topology is stuck; maybe a watchdog mechanism would be needed
2. "after all CB are JESD204_STATE_CHANGE_DONE", the JESD204 topology is
    ready to transition

Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

drivers/jesd204/jesd204-priv.h
@@ -21,6 +21,11 @@ enum jesd204_dev_state {
 	JESD204_STATE_INITIALIZED,
 	JESD204_STATE_PROBED,
 	JESD204_STATE_LINK_INIT,
+	JESD204_STATE_LINK_SUPPORTED,
+	JESD204_STATE_LINK_SETUP,
+	JESD204_STATE_CLOCKS_ENABLE,
+	JESD204_STATE_LINK_ENABLE,
+	JESD204_STATE_LINK_RUNNING,
 };
 
 /**
@@ -146,4 +151,8 @@ int jesd204_run_probe_states(struct jesd204_dev *jdev);
 
 int jesd204_run_states_to_init_link(struct jesd204_dev *jdev,
 				    enum jesd204_dev_state init_state);
+
+int jesd204_run_states_to_start_link(struct jesd204_dev *jdev,
+				     enum jesd204_dev_state init_state);
+
 #endif /* _JESD204_PRIV_H_ */

gtortone/XU5-linux-adi

(cherry picked from commit 772438298db3e98f01348ef12fc8871fe371c91c)

drivers/jesd204/jesd204-priv.h
@@ -21,6 +21,11 @@ enum jesd204_dev_state {
 	JESD204_STATE_INITIALIZED,
 	JESD204_STATE_PROBED,
 	JESD204_STATE_LINK_INIT,
+	JESD204_STATE_LINK_SUPPORTED,
+	JESD204_STATE_LINK_SETUP,
+	JESD204_STATE_CLOCKS_ENABLE,
+	JESD204_STATE_LINK_ENABLE,
+	JESD204_STATE_LINK_RUNNING,
 };
 
 /**
@@ -150,4 +155,8 @@ int jesd204_fsm_probe(struct jesd204_dev *jdev);
 
 int jesd204_fsm_init_links(struct jesd204_dev *jdev,
 			   enum jesd204_dev_state init_state);
+
+int jesd204_fsm_start_links(struct jesd204_dev *jdev,
+			    enum jesd204_dev_state init_state);
+
 #endif /* _JESD204_PRIV_H_ */